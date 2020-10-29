52e06933495aee1c38711b87b45f937f5e741434
savio591 <savio591@hotmail.com>
2020-10-29 17:24:20 -0300

    Primeira aula, revisada

--- GoStack/Nivel 1/01 - Criando Projeto Node/src/index.js
+++ GoStack/Nivel 1/01 - Criando Projeto Node/src/index.js
@@ -1,30 +1,74 @@
+/* importar express - criação de rotas e listening */
 const { request, response } = require("express");
 const express = require("express");
 
+/* importar uuid - gerador aleatório de uma id */
+const { uuid } = require("uuidv4");
+
+/* abreviar express */
 const app = express();
+app.use(express.json());
+/* Armazenando dados */
+const projects = [];
 
-app.get('/projects', (request, response) => {
-	return response.json([
-		'projeto 1',
-		'projeto 2'])
-	});
+app.get("/projects", (request, response) => {
+	const { title } = request.query;
+	const results = title
+	? projects.filter(project => project.title.includes(title))
+	: projects;
 
-app.post('/projects', (request, response) => {
-	return response.json([
-		'projeto 1',
-		'projeto 2'
-	]);
+
+	return response.json(results);
 });
 
-app.put('/projects', (request, response) => {
-	return response.json ([
-		'projeto 1', 
-		'projeto 2',
-		'projeto 3'
-	]);
+app.post("/projects", (request, response) => {
+	const { title, owner } = request.body;
+	console.log(title);
+	console.log(owner);
+
+	const project = { id: uuid(), title, owner };
+
+	projects.push(project);
+
+	return response.json(projects);
 });
 
+app.put("/projects/:id", (request, response) => {
+	const { id } = request.params;
+	const { title, owner } = request.body;
 
+	const projectIndex = projects.findIndex((project) => project.id == id);
 
+	if (projectIndex < 0) {
+		return response.status(400).json({ error: "Project not found" });
+	}
+
+	const project = {
+		id,
+		title,
+		owner,
+	};
+
+	projects[projectIndex] = project;
+
+	return response.json(project);
+});
+
+app.delete("/projects/:id", (request, response) => {
+	const { id } = request.params;
+
+	const projectIndex = projects.findIndex((project) => project.id == id)
+
+	if (projectIndex > 0) {
+		response.status(404).send("Project Not found")
+	}
+
+	projects.splice(projectIndex, 1)
+
+	return response.status(204).send("user " + id + "removed")
+})
+
+/* Ouvir em protocolo http */
 app.listen(3333, () => {
-console.log("Listening on 3333");});
+	console.log("Listening on 3333");
+});
