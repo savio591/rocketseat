08ca60d2261d02a75c9b076f30f443b80f523584
savio591 <savio591@hotmail.com>
2020-11-13 16:54:26 -0300

    Listando projetos de API

--- GoStack/Nivel 1/02 - Front-end com ReactJS/frontend/src/App.js
+++ GoStack/Nivel 1/02 - Front-end com ReactJS/frontend/src/App.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, {useState} from "react";
+
+import Header from "./components/header";
 
 function App() {
-    return <h1>Hello World</h1>
+
+const [projects, setProjects] = useState(["Desenvolvimento de app", "Front-End Web"])
+
+function handleAddProject() {
+
+  setProjects([...projects, 'Novo Projecto']);
+}
+
+  return (
+      //cria um bloco sem criar div
+    <>
+      <Header title="Homepeig">
+      {/* Uso de children */}
+          <ul>
+              <li>Aapo1</li>
+              <li>Aapo2</li>
+              <li>Aapo3</li>
+          </ul>
+      </Header>
+
+      {/* Uso de states */}
+      <Header title="Projequetz" />
+          <ul className="listaProjetos">
+            {projects.map(
+              function project(projects) {
+                return (<li key={projects}>{projects}</li>)
+              })}
+          </ul>
+
+          <button type="button" onClick={handleAddProject}>Add one more time</button>
+
+    </>
+  );
 }
 
 export default App;
