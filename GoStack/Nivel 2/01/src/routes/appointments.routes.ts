3e1ca28571b1e7ef0e2992c8095dccb92fe078d3
unknown <savio591@hotmail.com>
2020-11-30 16:15:57 -0300

    Nível 2

--- GoStack/Nivel 2/01/src/routes/appointments.routes.ts
+++ GoStack/Nivel 2/01/src/routes/appointments.routes.ts
@@ -1,11 +1,23 @@
 import { Router } from 'express';
+import { uuid } from 'uuidv4';
 
 const appointmentsRouter = Router();
 
 const appointments = [];
 
 appointmentsRouter.post('/', (request, response) => {
-  return response.json({'message': 'hello world'})
-})
+  console.log(request.body)
+  const { provider, date } = request.body;
 
-export default appointmentsRouter
+  const appointment = {
+    id: uuid(),
+    provider,
+    date,
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
--- GoStack/Nivel 2/01/src/server.ts
+++ GoStack/Nivel 2/01/src/server.ts
@@ -1,13 +1,13 @@
-import express, { json, response } from 'express'
+import express from 'express'
 import routes from './routes/index'
 
 const app = express()
 
+app.use(routes)
+app.use(express.json());
+
 const port = 3333
 
-app.get('/', ( Request, Response) => {
-    return Response.json('Hello World')
-})
 
 app.listen(port, () => {
     return (console.log(`Listenin' on localhost:${3333}`))
