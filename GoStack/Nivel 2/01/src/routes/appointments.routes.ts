447ec6d6f83957e01122c2085cd34c5fcce4180d
savio591 <savio591@hotmail.com>
2020-12-01 12:57:31 -0300

    0205 - Repositories

--- GoStack/Nivel 2/01/src/routes/appointments.routes.ts
+++ GoStack/Nivel 2/01/src/routes/appointments.routes.ts
@@ -1,37 +1,35 @@
-import { Router } from 'express';
-import { v4 as uuid } from 'uuid';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
+import { request, response, Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
-interface Appointment {
-  id: string,
-  provider: string,
-  date: Date
-}
+appointmentsRouter.get('/', (request, response) => {
+const appointments = appointmentsRepository.all()
 
-
-const appointments: Appointment[] = [];
+  return (
+    response.json(
+      appointments
+    )
+  )
+})
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parsedDate = startOfHour(parseISO(date));
-  const findAppointmentInSameDate = appointments.find(appointment =>
-    isEqual(parsedDate, appointment.date),
-  );
+
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate)
 
   if (findAppointmentInSameDate) {
     return response.status(400).json({"message": "This appointment is already booked"})
   }
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate,
-  };
+  const appointment = appointmentsRepository.create( {provider,date:parsedDate })
 
-  appointments.push(appointment);
 
   return response.json(appointment);
 });
