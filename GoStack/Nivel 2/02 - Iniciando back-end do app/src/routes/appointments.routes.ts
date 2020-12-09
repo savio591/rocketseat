27a54ea2a087770b95d72fb66bcf8863e73d58c3
savil experiments <savio591@hotmail.com>
2020-12-09 19:19:46 -0300

    Nivel 020201 - Model e Migration de Usuarios

--- GoStack/Nivel 2/02 - Iniciando back-end do app/src/routes/appointments.routes.ts
+++ GoStack/Nivel 2/02 - Iniciando back-end do app/src/routes/appointments.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { getCustomRepository } from "typeorm";
 import { parseISO } from 'date-fns';
 
 import CreateAppointmentService from '../services/CreateAppointmentService'
@@ -8,8 +9,9 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository';
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
 
-appointmentsRouter.get('/', (request, response) => {
-  const appointments = appointmentsRepository.all()
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository)
+  const appointments = await appointmentsRepository.find()
 
   return (
     response.json(
@@ -18,15 +20,15 @@ appointmentsRouter.get('/', (request, response) => {
   )
 })
 
-appointmentsRouter.post('/', (request, response) => {
+appointmentsRouter.post('/', async (request, response) => {
   try {
     const { provider, date } = request.body;
 
     const parsedDate = parseISO(date);
 
-    const createAppointment = new CreateAppointmentService(appointmentsRepository,)
+    const createAppointment = new CreateAppointmentService()
 
-    const appointment = createAppointment.execute({ date: parsedDate, provider });
+    const appointment = await createAppointment.execute({ date: parsedDate, provider });
 
     return response.json(appointment)
   }
--- GoStack/Nivel 2/02 - Iniciando back-end do app/src/services/CreateAppointmentService.ts
+++ GoStack/Nivel 2/02 - Iniciando back-end do app/src/services/CreateAppointmentService.ts
@@ -1,7 +1,7 @@
 import { startOfHour } from 'date-fns'
+import { getCustomRepository } from "typeorm";
 import Appointment from '../models/Appointment'
-import AppointmentRepository from '../repositories/AppointmentsRepository';
-
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 /**
  * [x] Recebendo das informações
  * [/] Tratativa de erros/excecções
@@ -20,23 +20,20 @@ interface Request {
 
 class CreateAppointmentService {
 
-  private appointmentsRepository: AppointmentRepository;
-
-  constructor(appointmentsRepository: AppointmentRepository) {
-    this.appointmentsRepository = appointmentsRepository
-  }
-
-  public execute({ date, provider }: Request): Appointment {
+  public async execute({ date, provider }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+    appointmentsRepository.create()
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(appointmentDate)
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(appointmentDate)
 
     if (findAppointmentInSameDate) {
       throw Error('This appointment is already booked')
     }
 
-    const appointment = this.appointmentsRepository.create({ provider, date: appointmentDate })
+    const appointment = appointmentsRepository.create({ provider, date: appointmentDate })
 
+    await appointmentsRepository.save(appointment)
     return appointment;
   }
 }
