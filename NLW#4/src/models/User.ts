642bef0369798029cc826860dea32c675b9abc2c
savio591 <savio591@hotmail.com>
2021-03-30 00:00:06 -0300

    2 - Criando Banco de Dados

---
+++ NLW#4/src/models/User.ts
@@ -0,0 +1,25 @@
+import { v4 as uuid } from "uuid";
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+
+@Entity('users')
+class User {
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { User };
